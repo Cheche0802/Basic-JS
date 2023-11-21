98994f4a7fba16a798e1f2681fa5cdbc132c2e5d
Jose Monterrey <e_jdmonterrey@stefanini.com>
2023-11-21 15:41:35 -0400

    formas de inicializar variables

--- seccion 2 intro y consola/app.js
+++ seccion 2 intro y consola/app.js
@@ -16,11 +16,20 @@ const c = 10;
 
 let z = 10;
 var y = 30;
-
-let d = 15;
+//opcion A
+/*let d = 15;
 let e = 15;
 let f = 15;
 let g = 15;
-let h = d + e ;
+let h = d + e ;*/
 
+//Opcion b
+/** let d = 15, e = 15, f = 15, g = 15, h = d + e ;*/
+
+//Opcion C
+let d = 15,
+    e = 15,
+    f = 15,
+    g = 15,
+    h = d + e 
 console.log(h);
