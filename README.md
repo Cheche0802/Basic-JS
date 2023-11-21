02e593f28f70d951030c7db92c4e3a928a06b1d2
Jose Monterrey <e_jdmonterrey@stefanini.com>
2023-11-21 15:30:23 -0400

    leccion 2

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # clase  N10 
 aqui se habla del hola mundo
 
+#clase 11
+introduccion a las variables
--- seccion 2 intro y consola/app.js
+++ seccion 2 intro y consola/app.js
@@ -1,6 +1,26 @@
 //clase N~10
 
 //Este sirve para imprimir en consola
-console.log("Hola");
+//console.log("Hola");
 //este es para imprimir en el HTML
-//document.write("<h1>Hola</h1>");
+//document.write("Hola munfo");
+
+//class n~ 11
+//inicializar variablres
+let a = 10;
+var b = 10;
+const c = 10;
+//c  = 20 // no se puede redifinir el valor de una constante
+
+//Diferencias
+
+let z = 10;
+var y = 30;
+
+let d = 15;
+let e = 15;
+let f = 15;
+let g = 15;
+let h = d + e ;
+
+console.log(h);
