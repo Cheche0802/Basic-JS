44c24a1255e2e5d43f6536ef586dacdec469008d
Jose Monterrey <jmonterrey@linkitve.com>
2023-12-21 10:27:49 -0400

    clase 52 solicitar una carta y borrarla del DECK

--- Seccion 5 laboratirio 1- Blackjack/assets/js/juego.js
+++ Seccion 5 laboratirio 1- Blackjack/assets/js/juego.js
@@ -8,9 +8,9 @@
 let deck = [];
 const typeOfCards = ['C', 'H', 'D', 'S'];
 const uniques = ['A', 'J', 'Q', 'K']
+
+
 // create the deck with all cards
-
-
 const crearDeck = () => {
     for( let i=2; i<= 10; i++){
         for (let type of typeOfCards) {
@@ -24,15 +24,28 @@ const crearDeck = () => {
       }
     }
 
-
-    console.log(deck);
-
+  //  console.log(deck);
     deck = _.shuffle(deck);
-
     console.log(deck);
-
     return deck;
 
 }
 
 crearDeck();
+
+// Esta funcion permite Pedir carta
+
+const pedirCarta = () => {
+
+    if (deck.length === 0){
+        throw 'No hay cartas en el deck';
+    }
+    const borrarCarta = deck.pop()
+    console.log (borrarCarta);
+    console.log(deck);
+}
+
+//ciclo para validar si el deck es igual a 0
+//for (let i in deck) {
+    pedirCarta();
+//}
