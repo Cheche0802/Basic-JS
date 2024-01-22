de8a240dc0eacdcff305490e7a0ab257d9c2c8ba
Jose Monterrey <jmonterrey@linkitve.com>
2024-01-22 14:03:58 -0400

    Clase 75 Sets y gets

--- Seccion 7 Clases en Javascript y ESNext private properties/assets/js/clases/class.js
+++ Seccion 7 Clases en Javascript y ESNext private properties/assets/js/clases/class.js
@@ -1,12 +1,20 @@
 
 
 class Persona {
+    //orden
+    //1 propiedad y metodos estaticos
+    //2 propiedades de la clase
+    //3 constructor
+    //4  set y gets
+    //5 metodos
+    //propiedades privadas
 
-    
         //Forma1
         nombre = '';
         codigo= '';
         frase= '';
+        comida = '';
+
     constructor (nombre ='Jose', codigo='008', frase= 'La comida es lo mas importante'){
 
         this.nombre = nombre;
@@ -15,6 +23,14 @@ class Persona {
     }
 
     //los metodos coumunmente van bajo el constructor
+
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita(){
+        return `La comida favorita de ${$this.nombre} es: ${this.comida}`;
+    }
 
     quienSoy(){
         console.log(`Soy ${this.nombre} y mi identidad es  ${this.codigo}`);
@@ -34,9 +50,10 @@ console.log(spiderman);
 console.log(ironman);
 
 spiderman.quienSoy();
-ironman.quienSoy();
-
 spiderman.miFrase();
 
-ironman.miFrase();
+spiderman.setComidaFavorita = 'Pie de cereza de la tia May';
+// Esto se puede hacer pero no es recomendado 
+spiderman.nemesis = 'Duende Verde';
 
+console.log(spiderman);
