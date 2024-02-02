9c597cafcbf0772ff5bc90b7263a7c1abfe315ad
Jose Monterrey <jmonterrey@linkitve.com>
2024-02-02 10:32:51 -0400

    clase 113 implemetacion de metodos al store

--- Seccion 10 Laboratorio Vite/todo-app/src/store/todo.store.js
+++ Seccion 10 Laboratorio Vite/todo-app/src/store/todo.store.js
@@ -21,7 +21,64 @@ const initStore= ()=> {
     console.log ("store initialized");
 }
 
+const  loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = (filter =  Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done)
+        default:
+            throw new Error(`Option ${filter} is not valid.`);
+            break;
+    }
+}
+
+/**
+ * @params {String} descriptions
+ */
+const addTodo = (description) => {
+    if (!description) throw new Error('Description is required');
+
+    state.todos.push(new Todo(description));
+}
+
+const toggleTodo = (todoid) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoid) => {
+    state.todos = state.todos.filter(todo => todo.id != todoid)
+}
+
+const deleteComplete = () => {
+    state.todos = state.todos.filter(todo => todo.done)
+}
+
+
+const setFilter= (newFilter =  Filters.All) => {
+
+   state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+   return state.filter
+}
+
 
 export default {
+    getTodos,
     initStore,
+    loadStore,
+    addTodo,
+    toggleTodo, 
+    deleteTodo, 
+    deleteComplete,
+    setFilter,
+    getCurrentFilter
 }
